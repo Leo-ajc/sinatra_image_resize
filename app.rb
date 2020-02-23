5ad78c1dae83f3eddddec41f72a50927af9d55f7
Leo Campbell <leo.campbell@gmail.com>
2020-02-23 20:03:27 +0000

    Both endpoints, #1 image metadata #2 resizing an image 25%, WORKING! Both with green tests.

--- app.rb
+++ app.rb
@@ -4,6 +4,22 @@ Bundler.require(Sinatra::Base.environment)  # load all the environment specific 
 set :port, 3333
 set :bind, '0.0.0.0'
 
+get '/info' do
+  content_type :json
+  # MiniMagick::Image.open uses Tempfile, no file system access.
+  MiniMagick::Image.open(params['url']).data.to_json
+end
+
+get '/thumbnail' do
+  # MiniMagick::Image.open uses Tempfile, no file system access.
+  image = MiniMagick::Image.open params['url']
+  image.resize "25%"
+  send_file open(image.path,
+    type: image.data["mimeType"],
+    disposition: 'inline'
+  )
+end
+
 get '/' do
   'Hello World!'
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -9,6 +9,42 @@ RSpec.describe 'App', type: :request do
     Sinatra::Application
   end
 
+  let(:dummy_image_path) do
+    'spec/fixtures/dummy_image.jpg'
+  end
+  let(:dummy_image_url_stubbed) do
+    'https://i.imgur.com/fIokC3D.jpg'
+  end
+
+  before(:each) do
+    stub_request(:get, /imgur.com/).
+      with(
+        headers: {'Accept'=>'*/*', 'User-Agent'=>'Ruby'}).
+      to_return(
+        status: 200,
+        body: File.read(dummy_image_path),
+        headers: {}
+      )
+  end
+
+  it 'displays image meta data' do
+    get '/info', url: dummy_image_url_stubbed
+    # The http request is stubbed with the identical
+    # dummy image. Testing the content-type is the
+    # only meaningful test of behaviour here.
+    expect(last_response.content_type).to eq("application/json")
+  end
+
+  it 'resizes dummy image to 25%' do
+    get '/thumbnail', url: dummy_image_url_stubbed
+    expect(last_response.content_type).to eq("image/jpeg")
+
+    dummy_image = MiniMagick::Image.open(dummy_image_path)
+    response_image = MiniMagick::Image.read(last_response.body)
+
+    expect(response_image.width).to eq dummy_image.width * 0.25
+  end
+
   it 'says hello world' do
     get '/'
     expect(last_response.body).to match(/Hello World/)
